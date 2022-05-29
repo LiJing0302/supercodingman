5b35bfd006051bbf6aea588fef5fe660dc0a3328
lijing <lijing9932@163.com>
2022-05-29 22:23:54 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+# npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -16,4 +16,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:lijing0302/supercodingman.git master:gh-pages
 
-cd -
+# cd -
