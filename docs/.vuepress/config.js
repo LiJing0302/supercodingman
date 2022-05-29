9586bd4304dbd3bb0c0cdb9dd24871df83d30b48
lijing <lijing9932@163.com>
2022-05-29 21:40:05 +0800

    bugfix config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,15 +29,6 @@ module.exports = {
                     { title: "学前必读", path: "/" }
                 ]
             },
-            {
-              title: "基础学习",
-              path: '/handbook/ConditionalTypes',
-              collapsable: false, // 不折叠
-              children: [
-                { title: "条件类型", path: "/handbook/ConditionalTypes" },
-                { title: "泛型", path: "/handbook/Generics" }
-              ],
-            }
           ]
     }
   }
